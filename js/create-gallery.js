0f4655989033257f3d75f09a8e47109ee612568a
JuliaZaykaS <sys55@mail.ru>
2021-05-17 15:01:28 +0300

    8 дз продолжение
    сделала закрытие по эскейп, открытие большого изображения в модалке

--- js/create-gallery.js
+++ js/create-gallery.js
@@ -26,16 +26,22 @@ function createGallery(images) {
 };
 
 galleryListEl.addEventListener('click', OnImageOfGalleryClick);
+
 function OnImageOfGalleryClick(event) {
     const isImageEl = event.target.classList.contains('gallery__image');
     if (!isImageEl) return;
 
     const imageEl = event.target;
-    const imageLargeUrl = imageEl.dataset.source;
-    console.log(imageEl);
-    console.log(imageLargeUrl);
-    // return imageLargeUrl;
-    openModal();
+  const imageLargeUrl = imageEl.dataset.source;
+  window.addEventListener('keydown', closeModalByEscape);
+  window.addEventListener('keydown', turnImagesRight);
+  window.addEventListener('keydown', turnImagesLeft);
+
+  openModal();
+
+  imageEl.src = imageLargeUrl;
+
+  showModalImage(imageEl);
 
 
 }
@@ -44,22 +50,61 @@ const modalEl = document.querySelector('.js-lightbox');
 const modalCloseBtnEl = document.querySelector('[data-action="close-lightbox"]');
 
 function openModal() {
-    modalEl.classList.add('is-open');
-
-
+  modalEl.classList.add('is-open');
 
 }
 
-modalCloseBtnEl.addEventListener('click', closeModal)
+const imageModalEl = document.querySelector('.lightbox__image');
+
+modalCloseBtnEl.addEventListener('click', closeModal);
+
+// const modalOverlay = document.querySelector('.lightbox__overlay');
+modalEl.addEventListener('click', OnOverlayClick);
+
+function OnOverlayClick(event) {
+  const isOverlayEl = event.target.classList.contains('lightbox__overlay');
+  if (!isOverlayEl) return;
+  closeModal();
+
+}
+
+
+
+function closeModalByEscape(event) {
+  if (event.code === 'Escape') {
+    closeModal();
+}
+
+}
+
+function turnImagesRight(event) {
+  if (event.code === 'ArrowRight') {
+    console.log(event.code);
+    // closeModal();
+  }
+
+}
+function turnImagesLeft(event) {
+  if (event.code === 'ArrowLeft') {
+    console.log(event.code);
+    // closeModal();
+}
+
+}
+
+
 function closeModal() {
-    modalEl.classList.remove('is-open')
+  window.removeEventListener('keyup', closeModalByEscape);
+  window.removeEventListener('keyup', turnImagesRight);
+  window.removeEventListener('keyup', turnImagesLeft);
+  modalEl.classList.remove('is-open');
+  imageModalEl.src = '';
 
 }
 
 
-// galleryListEl.addEventListener('click', getUrlOfBigImage);
-
-// function getUrlOfBigImage() {
-//     const UrlOfBigImage =
-// }
+function showModalImage(image) {
+  imageModalEl.src = image.dataset.source;
+  imageModalEl.alt = image.alt;
+};
 
